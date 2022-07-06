a8ea642cc693635dbea2344434612033cbb43e97
elbarqy <elbarqy.dev@gmail.com>
2022-07-06 11:05:06 +0200

    poc

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { connect as connectToEventStore } from './eventstore.db';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  await connectToEventStore();
   await app.listen(3000);
 }
 bootstrap();
---
+++ app/src/views.ts
@@ -0,0 +1,40 @@
+import { FORWARDS, START } from '@eventstore/db-client';
+import { client } from './eventstore.db';
+
+interface Purchase {
+  purchaseId: string;
+  name: string;
+  amount: string;
+  wasRefunded: boolean;
+}
+const getAllPurchases = async () => {
+  const purchases: Purchase[] = [];
+  const getPurchaseById = createGetPurchaseById(purchases);
+  const events = client.readAll({
+    direction: FORWARDS,
+    fromPosition: START,
+    maxCount: 1000,
+  });
+  for await (const { event } of events) {
+    const data: any = event.data;
+    switch (event?.type) {
+      case 'ProductPurchased':
+        purchases.push({
+          purchaseId: data.purchaseId,
+          amount: data.amount,
+          name: data.name,
+          wasRefunded: false,
+        });
+        break;
+      case 'ProductRefunded':
+        getPurchaseById(data.purchaseId).wasRefunded = true;
+        break;
+    }
+  }
+  return purchases;
+};
+
+const createGetPurchaseById = (purchases: Purchase[]) => (purchaseId: string) =>
+  purchases.filter((p) => p.purchaseId == purchaseId)[0];
+
+export { getAllPurchases, Purchase };
